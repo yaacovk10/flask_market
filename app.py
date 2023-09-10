5fdd0fb22441fc5c93710327e0670dd214b7d162
unknown <yaacovana@gmail.com>
2023-09-10 23:31:43 +0300

    modifications to supermarket

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect
+import logging
+
+
 
 app = Flask(__name__)
 
-market = [{'name': 'cola', 'price' : 10},
-          {'name':'cafe', 'price': 20},
-          {'name':'tea', 'price': 5},
-          {'name':'apple', 'price': 2},
-            {'name':'milk', 'price': 6},
-              {'name':'yogourt', 'price': 1},
-                {'name':'xl', 'price': 15},  ]
+market = [{'name': 'cola', 'price': 10},
+          {'name': 'coffee', 'price': 20},
+          {'name': 'tea', 'price': 5},
+          {'name': 'apple', 'price': 2},
+          {'name': 'milk', 'price': 6},
+          {'name': 'yogourt', 'price': 1},
+          {'name': 'xl', 'price': 15}]
 
 cart = []
 total_sum = 0
 
 @app.route('/')
+def welcome():
+    return render_template("welcome.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/shop')
 def index():
+    return render_template("index.html", market=market, cart=cart, total_sum=total_sum)
+
+@app.route('/buy/<int:product_index>')
+def buy_product(product_index):
     global total_sum
-    product_index = request.args.get('prod')
-    print (product_index)
-    if product_index:
-        print(market[int(product_index)])
-        cart.append(market[int(product_index)]['name'])
-        total_sum += market[int(product_index)]['price']
-        print(f"total is {total_sum}")
-    return render_template ("index.html", list = market, product = product_index, cart = cart, total_sum = total_sum)
+    if 0 <= product_index < len(market):
+        name = market[product_index]['name']
+        item_in_cart = next((item for item in cart if item['name'] == name), None)
+        if item_in_cart:
+            item_in_cart['amount'] += 1
+        else:
+            cart.append({"name": name, "amount": 1})
+        total_sum += market[product_index]['price']
+    return redirect('/shop')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
